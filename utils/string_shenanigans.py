5f1fc3c9829aba2d7c53c0e7b96fa9ccc27f0ba0
Ruben Kaiser <ruben@kaiser.fyi>
2024-06-28 02:23:32 +0200

    fix(utils/string_shenanigans): Improved clean_commit_message to remove leading/trailing newline characters and extra whitespace

--- utils/string_shenanigans.py
+++ utils/string_shenanigans.py
@@ -3,7 +3,8 @@ import re
 
 def clean_commit_message(commit_message):
     # Remove leading backticks, asterisks, and newline characters
-    cleaned_message = re.sub(r'^[`*]{1,4}\n?', '', commit_message)
+    # cleaned_message = re.sub(r'^[`*]{1,4}\n?', '', commit_message)
+    cleaned_message = re.sub(r'[\n`*]{1,3}', '', commit_message)
 
     # Strip any leading or trailing whitespace
     cleaned_message = cleaned_message.strip()
